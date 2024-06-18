218d5d513dd88e55ce2e112eaf074d5562ee64aa
Fabicca86 <165907092+Fabicca86@users.noreply.github.com>
2024-06-18 01:17:01 -0300

    erasing unnecessary comments

--- teste/main.py
+++ teste/main.py
@@ -1,8 +1,8 @@
 from perguntas import perguntas
 
-nome = (input ('OLÁ, SEJA BEM VINDS! PARA COMEÇAR, QUAL É O SEU NOME? '))       #definir str para input
+nome = (input ('OLÁ, SEJA BEM VINDS! PARA COMEÇAR, QUAL É O SEU NOME? '))       #personalization of test with user name
 nome = nome.upper
-print(nome, ' VOU TE EXPLICAR COMO FUNCIONA.')
+print(nome, ' VOU TE EXPLICAR COMO FUNCIONA.') #first simple orientations
 print('''
       AS FRASES A SEGUIR NOS AJUDARÃO A ENTENDER O QUE TE MOTIVA A ESTUDAR.
       RESPONDA COM CALMA POIS NÃO PODERÁ MUDAR A RESPOSTA
@@ -11,33 +11,30 @@ print('''
         B - PARA AS VEZES
         C - PARA NUNCA')
       ''')
-#código usando listas tuplas e dicionário
-#lista de perguntas
-#for pergunta in perguntas):
- #   print(pergunta)
-#perguntas  = ['pergunta 1', 'pergunta 2','pergunta 3','pergunta 4','pergunta 5','pergunta 6','pergunta 7','pergunta 8','pergunta 9','pergunta 10','pergunta 11','pergunta 12','pergunta 13','pergunta 14','pergunta 15','pergunta 16','pergunta 17','pergunta 18','pergunta 19','pergunta 20','pergunta 21','pergunta 22','pergunta 23','pergunta 24','pergunta 25','pergunta 26','pergunta 27','pergunta 28','pergunta 29','pergunta 30','pergunta 31']
+#código usando listas, loops e dicionário
+
 #Dicionário para armazenar as respostas
 respostas = { }
 #opção de respostas
 opcoes = {'SEMPRE', 'AS VEZES', 'NUNCA'}
 #contador de pontos
 pontos = 0
-#LOOP das perguntas
-for i , pergunta in enumerate(perguntas): #teste alterando o i
+#LOOP for the questions
+for i , pergunta in enumerate(perguntas): # iteract with questions because they have diferent values from 1 to 17 and 18 to 31
     #definindo pontuação diferente
     if i <18 :
         opcoes = {'SEMPRE':3, 'AS VEZES':2, 'NUNCA':1}
     else:
          opcoes = {'SEMPRE':1, 'AS VEZES':2, 'NUNCA':3}
     resposta = ' '
-    while resposta not in opcoes.keys():
+    while resposta not in opcoes.keys(): # loop condition for case the user don't respond well
         resposta = input(pergunta +' (sempre / as vezes / nunca): ').upper()
         if resposta not in opcoes.keys():
             print('resposta inválida! Por favor responda com SEMPRE, AS VEZES OU NUNCA')
-    respostas[pergunta] = resposta
+    respostas[pergunta] = resposta    #saving the answers
     #Contador dos pontos
     pontos += opcoes[resposta]
 #Imprimir as respostas e pontuação total
-for pergunta, resposta in respostas.items():
+for pergunta, resposta in respostas.items(): #show options to user
     print (f'{pergunta}: {resposta}')
-print (f'Pontuação total: {pontos}')
+print (f'Pontuação total: {pontos}') #this should not be messaged for user, so after when improving the code to do a graph i will remove it, just to see if count is OK
